ab18d20261ba8d8d7cd583d7d6a0f2b07159da53
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-07-18 00:26:11 +0530

    made changes

--- client/src/components/student/GallerySlider.jsx
+++ client/src/components/student/GallerySlider.jsx
@@ -36,7 +36,7 @@ const GallerySlider = () => {
 
   return (
     <div className="w-full max-w-4xl mx-auto my-8">
-      <h2 className="text-3xl font-bold text-blue-700 mb-6 text-center">Gallery</h2>
+      <h2 className="text-3xl font-bold text-black mb-6 text-center">Gallery</h2>
       <Slider {...settings}>
         {galleryImages.map(item => (
           <div key={item._id} className="px-2">
